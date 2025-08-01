1ba4399f7e4e387e14204ab8ba01c7f81cf60e9a
thezaieem <izaim04@outlook.com>
2025-08-01 14:41:41 +0100

    unpush .env

--- netlify/functions/checkFartlist.js
+++ netlify/functions/checkFartlist.js
@@ -10,14 +10,15 @@ exports.handler = async (event) => {
     };
   }
 
-  const client = new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: { rejectUnauthorized: false }
-  });
+ const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
+});
+
 
   try {
     await client.connect();
